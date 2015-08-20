4b1c9dec0e93f8f019c408ac883d07e4499b6dec
Silas Barta <sbarta@gmail.com>
2015-08-19 19:11:35 -0700

    Basic REST working

---
+++ api/tests.py
@@ -0,0 +1,10 @@
+import json
+
+from django.test import TestCase
+
+# Create your tests here.
+from rest_framework.test import APIRequestFactory
+from rest_framework.test import APITestCase
+
+# Using the standard RequestFactory API to create a form POST request
+
